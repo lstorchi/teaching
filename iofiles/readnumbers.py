545359e7529a7b8ba14a954a4317e6c7b067ce6b
lstorchi <loriano@storchi.org>
2016-12-28 09:25:59 +0100

    using a function

--- iofiles/readnumbers.py
+++ iofiles/readnumbers.py
@@ -1,6 +1,40 @@
 import numpy
 import math
 
+########################################################
+
+def mean_stdev_welford (values):
+  
+  m = 0.0 
+
+  if len(values) == 1:
+    m = values[0] 
+ 
+  s = 0.0
+
+  # vedi il Knuth "Art of Computer Programming"
+  if len(values) >= 1:
+    moldm = values[0]
+    mnewm = values[0]
+    molds = 0.0
+    mnews = 0.0
+    i = 0
+
+    for i in range(1, len(values)):
+      x = values[i]
+      mnewm = moldm + (x - moldm)/(i+1)
+      mnews = molds + (x - moldm)*(x - mnewm)
+    
+      moldm = mnewm 
+      molds = mnews
+  
+    s = math.sqrt(mnews/(i+1))
+    m = mnewm
+
+  return m, s
+
+########################################################
+
 f = open("numbers.txt", "r")
 
 values = []
@@ -14,24 +48,9 @@ for l in f:
   else:
     print "error while reading line: ", i
 
-# vedi il Knuth "Art of Computer Programming"
-if len(values) >= 1:
-  moldm = values[0]
-  mnewm = values[0]
-  molds = 0.0
-  mnews = 0.0
-  i = 0
-
-  for i in range(1, len(values)):
-    x = values[i]
-    mnewm = moldm + (x - moldm)/(i+1)
-    mnews = molds + (x - moldm)*(x - mnewm)
-    
-    moldm = mnewm 
-    molds = mnews
-
-  print "media: ", mnewm, " ", numpy.mean(values)
-  print "stdev: ", math.sqrt(mnews/(i+1)), " ", numpy.std(values) # diviso n = len(values)
+m, s = mean_stdev_welford (values)
+print "media: ", m, " ", numpy.mean(values)
+print "stdev: ", s, " ", numpy.std(values) # diviso n = len(values)
 
 # classico con rischio di overflow 
 if len(values) >= 1:
