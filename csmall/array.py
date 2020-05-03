74bc12847e23aad6e228171ae58505f32a4278f3
Loriano Storchi <loriano@storchi.org>
2020-05-03 17:30:33 +0200

    minor

---
+++ csmall/array.py
@@ -0,0 +1,20 @@
+import random
+
+N = 20
+
+a = []
+b = []
+
+for i in range(N):
+    a.append(random.randrange(1, 30))
+    b.append(random.randrange(1, 30))
+
+for i in range(N):
+    print("%3d %3d"%(a[i], b[i]))
+
+s = 0.0
+for i in range(10):
+    s = s + float(a[i]*b[i])
+
+print("")
+print("S = %.5e"%s)
