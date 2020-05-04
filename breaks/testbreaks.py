31058ea32bada9acf3829804ff5392afbfe8bd91
Loriano Storchi <loriano@storchi.org>
2020-05-04 11:55:42 +0200

    backup

--- breaks/testbreaks.py
+++ breaks/testbreaks.py
@@ -1,7 +1,5 @@
-for i in range(10):
-    print("loop 1: ", i)
-    for j in range(10):
-        print("    loop 2: ", j)
-        if (j > 5):
-            break;
+for i in range(0,10):
+    print("i: ", i)
+    if i > 5:
+        break;
 
---
+++ breaks/testbreaks1.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print("loop 1: ", i)
+    for j in range(10):
+        print("    loop 2: ", j)
+        if (j > 5):
+            break;
+
