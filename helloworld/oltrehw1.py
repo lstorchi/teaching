b4412217628d0d00a7795464e6829ee08dc47a7a
Loriano Storchi <loriano@storchi.org>
2020-05-04 11:18:59 +0200

    backup

--- helloworld/oltrehw1.py
+++ helloworld/oltrehw1.py
@@ -6,7 +6,7 @@ if z == 3.45 or y == "Hello":
     x = x + 1
     y = y + " World"
 
-print(("Valore di x: ", x))
-print(("Valore di y: ", y))
+print("Valore di x: ", x)
+print("Valore di y: ", y)
 
 print(z)
--- helloworld/oltrehw4.py
+++ helloworld/oltrehw4.py
@@ -1,9 +1,25 @@
-si = eval(input ("inserisci un numero: " ))
+si = input ("inserisci un numero: " )
 i = float(si)
 
 if i < 0 :
     print("numero inferiore a zero") 
 elif i == 0:
+    print("inseiro uguale a zero")
+else:
+    print("numero maggiore di zero")
+
+
+si = eval(input ("inserisci un numero: " )) # The expression argument 
+                                            # is parsed and evaluated as a 
+                                            # Python expression (technically 
+                                            # speaking, a condition list) 
+                                            # using the globals and locals 
+                                            # dictionaries as global and local 
+                                            # namespace. 
+
+if si < 0 :
+    print("numero inferiore a zero") 
+elif si == 0:
     print("inseiro uguale a zero")
 else:
     print("numero maggiore di zero")
