ec2fb61adaf060db436e4f00c8a077be182cf90b
Loriano Storchi <loriano@storchi.org>
2020-05-04 11:34:57 +0200

    backup

--- secondord/solv.py
+++ secondord/solv.py
@@ -1,27 +1,17 @@
 import math
 
-ai = eval(input("insert a:")) 
-a = float(ai)
-
-bi = eval(input("insert b:"))
-b = float(bi)
-
-ci = eval(input("insert c:"))
-c = float(ci)
+a = float(input("insert a:")) 
+b = float(input("insert b:"))
+c = float(input("insert c:"))
 
 print("a = ", a, " b = ", b, " c = ", c)
 
 delta = math.pow(b, 2.0) - (4.0 * a * c)
 
 if (delta >= 0):
-
   tn = math.sqrt(delta)
-
   sol1 = ((-1.0 * b) + tn) / (2.0 * a)
   sol2 = ((-1.0 * b) - tn) / (2.0 * a)
-
   print(sol1, sol2)
-
 else:
-
   print("No real solutions")
